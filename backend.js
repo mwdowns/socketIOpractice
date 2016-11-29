3efd170d4941841d2b982c696edb78491c1aff79
Matthew Downs <mwdowns@gmail.com>
2016-11-29 17:06:56 -0500

    adds code for different rooms for different types of user

--- backend.js
+++ backend.js
@@ -2,39 +2,89 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var users = [];
+var room = '';
+var listeners = [];
+var speakerRoom = [];
 
 app.use(express.static('public'));
 
 io.on('connection', function(socket) {
+  socket.on('user', function(data) {
+    if (data.listener) {
+      console.log('this user is a listener');
+      console.log('this is the listeners array and should be empty', listeners.map(function(socket){
+        return socket.nickname;
+      }));
+      if (data.username in listeners) {
+        console.log('name taken');
+      }
+      else {
+        socket.nickname = data.username;
+        socket.room = 'listeners';
+        listeners.push(socket);
+        console.log('this is the listeners array and should be populated now', listeners.map(function(socket){
+          return socket.nickname;
+        }));
+        socket.join(socket.room);
+        updateListeners();
+      }
+      socket.on('user', function(data) {
+        console.log('blah');
+      });
+    }
+    else {
+      console.log('this user is a speaker');
+      socket.nickname = data.username;
+      socket.room = data.username;
+      speakerRoom.push(socket);
+      console.log('hey ho, lets go. im speaker, this is the room name', socket.room);
+      console.log('this is the list of available listeners', listeners.map(function(socket){
+        return socket.nickname;
+      }));
+      socket.join(socket.room);
+      io.emit('user room update', socket.room);
+      updateSpeakerRoom();
+      if (listeners.length > 0) {
+        console.log('the listeners list is populated!');
+      }
+    }
+
+  });
   console.log('a user connected');
+
+  function updateListeners() {
+    io.emit('sent listeners', listeners.map(function(socket){
+      return socket.nickname;
+    }));
+  }
+
+  function updateSpeakerRoom() {
+    io.emit('sent users', speakerRoom.map(function(socket) {
+      return socket.nickname;
+    }));
+  }
+
+  socket.on('sent chat message', function(msg) {
+    console.log('sent chat message:' + msg);
+    io.sockets.in(socket.room).emit('recieved chat message', {message: msg, user: socket.nickname});
+  });
+
   socket.on('disconnect', function() {
     console.log('user disconnected');
     if(!socket.nickname) return;
-    users.splice(users.indexOf(socket.nickname), 1);
-    updateNickname();
+    var index = listeners.indexOf(socket);
+    var index2 = speakerRoom.indexOf(socket);
+    if (index != -1) {
+      listeners.splice(index, 1);
+    }
+    if (index2 != -1) {
+      speakerRoom.splice(index2, 1);
+    }
+    updateListeners();
   });
 
-  function updateNickname() {
-    io.emit('usernames', users);
-  }
-
-  socket.on('new user', function(data, callback) {
-    if (users.indexOf(data) != -1) {
-      callback(false);
-    }
-    else {
-      callback(true);
-      socket.nickname = data;
-      users.push(socket.nickname);
-      updateNickname();
-    }
-  });
-  socket.on('sent chat message', function(msg) {
-    console.log('sent chat message:' + msg);
-    io.emit('recieved chat message', {message: msg, user: socket.nickname});
-  });
 });
+
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
--- public/frontend.js
+++ public/frontend.js
@@ -1,3 +1,15 @@
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 $(function() {
 var socket = io();
 var $messageForm = $('#sendMessage');
@@ -6,40 +18,79 @@ var $chat = $('#messages');
 var $nickForm = $('#setNick');
 var $nickError = $('#nickError');
 var $nick = $('#nickname');
+var nickname = '';
+var user = {username: getParameterByName('username'), listener: getParameterByName('listener') === 'true'};
 
-$nickForm.submit(function(e) {
-  e.preventDefault();
-  console.log('hello');
-  socket.emit('new user', $nick.val(), function(data) {
-    if (data) {
-      $('#nickWrap').hide();
-      $('#contentWrap').show();
+function connectUser() {
+  socket.on('connect', function() {
+    socket.emit('user', user);
+    console.log('this is the user', user);
+  });
+}
+
+
+function updateListenerList() {
+  socket.on('sent listeners', function(users) {
+    console.log('these are the listeners in the room', users);
+    var chatusers = '';
+    for (var i = 0; i < users.length; i++) {
+      chatusers += users[i] + '<br>';
     }
-    else {
-      $nickError.html('Please choose another nickname');
+    $('#userlist').html(chatusers);
+  });
+}
+
+function updateSpeakerRoomList() {
+  socket.on('sent users', function(users) {
+    console.log('these are the speakers online', users);
+    var chatusers = '';
+    for (var i = 0; i < users.length; i++) {
+      chatusers += users[i] + '<br>';
+    }
+    $('#userlist').html(chatusers);
+  });
+}
+
+function emitMessage() {
+  socket.emit('sent chat message', $message.val());
+  $chat.append("<li><b>" + user.username + "</b>" +  ": " + $message.val() + "</li>");
+  $message.val('');
+}
+
+function roomWelcomeMessage() {
+  socket.on('user room update', function(roomName) {
+    $chat.append("<li>Welcome to <b>" + roomName + "</b>'s room");
+  });
+}
+
+if (user.listener) {
+  console.log('this user is a listener');
+  socket.emit('create', 'listeners');
+  connectUser();
+  updateListenerList();
+}
+else {
+  console.log('this user is a speaker');
+  socket.emit('create', user.username);
+  connectUser();
+  socket.on('user room update', function(roomName) {
+    console.log('hi');
+    if (roomName === user.username) {
+      $chat.append("<li>Welcome to <b>" + roomName + "</b>'s room");
     }
   });
-  $nick.val('');
-});
-
-socket.on('usernames', function(users) {
-  console.log(users.length);
-  var chatusers = '';
-  for (var i = 0; i < users.length; i++) {
-    chatusers += users[i] + '<br>';
-  }
-  $('#userlist').html(chatusers);
-});
+  updateSpeakerRoomList();
+}
 
 $messageForm.submit(function(e){
   e.preventDefault();
-  socket.emit('sent chat message', $message.val());
-  $message.val('');
-  return false;
+  emitMessage();
 });
 
 socket.on('recieved chat message', function(data){
-  $chat.append("<li><b>" + data.user + "</b>" +  ": " + data.message + "</li>");
+  if (user.username != data.user) {
+    $chat.append("<li><b>" + data.user + ":</b> " + data.message + "</li>");
+  }
 });
 
 });
